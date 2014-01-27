66a145626d2671dea456f0b1a4795fadf109f54e
Jacob Hladky <jacob.hladky@gmail.com>
2014-01-27 13:00:09 -0800

    Better css

--- js/hyperboleon.js
+++ js/hyperboleon.js
@@ -8,7 +8,7 @@ var hyperboleon = {
     $contact: $("#contact"),
     $contactList: $("#contactList"),
     $sectionList: $("#sectionList"),
-    
+
     sections: ["about", "projects", "art", "else"],
     sectionCtrls: {},
     sectionTitles: {},
@@ -25,7 +25,7 @@ hyperboleon.setup = function () {
         self.sectionTitles[section] = $("<div>").text(section.toUpperCase());
         self.sectionContents[section] = $("<div>");
     });
-    
+
     this.sectionTitles.about
         .attr("id", "aboutTitle")
         .css("background-image", "url('img/me.jpg')");
@@ -36,45 +36,7 @@ hyperboleon.setup = function () {
     $.each(this.sectionCtrls, function () {
         self.$sectionList.append(this);
     });
-    
-    $(window).resize(function () {
-        var width = $(this).width();
 
-        if(width < 900) {
-            if(width < 650) {
-                self.$subtitle.addClass("hidden");
-            } else {
-                self.$subtitle.removeClass("hidden");
-            }
-            self.$header
-                .add(self.$footer)
-                .css("margin-left", "-" + width / 2 + "px");
-            self.$title.css("font-size", width / 11.25 + "pt");
-            self.$contactList.css("right", "0px");
-            self.$sectionList.addClass("mobile").removeClass("normal");
-        } else {
-            if(width < 1200) {
-                self.$sectionList.addClass("mobile").removeClass("normal");
-            } else {
-                self.$sectionList.addClass("normal").removeClass("mobile");
-                self.$sectionList
-                    .css("left", (width - 900) / 2 - self.$sectionList.width());
-            }
-            self.$header.add(self.$footer).css("margin-left", "-450px");
-            self.$title.css("font-size", "80pt");
-            self.$contactList.css("right", (width - 900) / 2)
-        }
-        $("#shadow").css("left", (width - $("#shadow").width()) / 2);
-    });
-
-    $("body")[0].addEventListener ("overflowchanged", function() {
-        var width = $(window).width();
-
-        $("#shadow").css("left", (width - $("#shadow").width()) / 2);
-        self.$contactList.css("right", (width - 900) / 2);
-        self.$sectionList.css("left", (width - 900) / 2 - self.$sectionList.width());
-    }, false);
-    
     this.$contact.click(function () {
         $(this).children().toggleClass("hidden");
         self.$contactList.slideToggle();
@@ -93,8 +55,45 @@ hyperboleon.setup = function () {
             self.$titles.append(self.activeTitle);
             self.$contents.append(self.activeContent);
         });
-    }); 
-    
+    });
+
+    $(window).resize(function () {
+        var width = $(this).width();
+        
+        if(width < 900) {
+            if(width < 700) {
+                self.$subtitle.addClass("hidden");
+            } else {
+                self.$subtitle.removeClass("hidden");
+            }
+            self.$header.css("margin-left", "-" + width / 2 + "px");
+            self.$title.css("font-size", width / 11.25 + "pt");
+            self.$contactList.css("right", "0px");
+            self.$sectionList.addClass("mobile").removeClass("normal");
+        } else {
+            if(width < 1200) {
+                self.$sectionList.addClass("mobile").removeClass("normal");
+            } else {
+                self.$sectionList.addClass("normal").removeClass("mobile");
+                self.$sectionList
+                    .css("left", (width - 900) / 2 - self.$sectionList.width());
+            }
+            self.$header.css("margin-left", "-450px");
+            self.$title.css("font-size", "80pt");
+            self.$contactList.css("right", (width - 900) / 2);
+        }
+        
+        $("#shadow").css("left", (width - $("#shadow").width()) / 2);
+    });
+
+    /*$("body")[0].addEventListener ("overflowchanged", function() {
+        var width = $(window).width();
+
+        $("#shadow").css("left", (width - $("#shadow").width()) / 2);
+        self.$contactList.css("right", (width - 900) / 2);
+        self.$sectionList.css("left", (width - 900) / 2 - self.$sectionList.width());
+    }, false); */
+
     this.activeContent = this.sectionContents.about;
     this.activeTitle = this.sectionTitles.about;
     this.sectionCtrls["about"].click();
