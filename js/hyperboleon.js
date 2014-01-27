a291c28808af73e932038f2abe9db8be7dee3ca0
Jacob Hladky <jacob.hladky@gmail.com>
2014-01-27 15:24:45 -0800

    Much better css

--- js/hyperboleon.js
+++ js/hyperboleon.js
@@ -4,8 +4,6 @@ var hyperboleon = {
     $title: $("#title"),
     $titles: $("#titles"), //rename me
     $contents: $("#content"), //rename me
-    $subtitle: $("#subtitle"),
-    $contact: $("#contact"),
     $contactList: $("#contactList"),
     $sectionList: $("#sectionList"),
 
@@ -58,45 +56,13 @@ hyperboleon.setup = function () {
     });
 
     $(window).resize(function () {
-        var width = $(this).width();
-        
-        if(width < 900) {
-            if(width < 700) {
-                self.$subtitle.addClass("hidden");
-            } else {
-                self.$subtitle.removeClass("hidden");
-            }
-            self.$header.css("margin-left", "-" + width / 2 + "px");
-            self.$title.css("font-size", width / 11.25 + "pt");
-            self.$contactList.css("right", "0px");
-            self.$sectionList.addClass("mobile").removeClass("normal");
-        } else {
-            if(width < 1200) {
-                self.$sectionList.addClass("mobile").removeClass("normal");
-            } else {
-                self.$sectionList.addClass("normal").removeClass("mobile");
-                self.$sectionList
-                    .css("left", (width - 900) / 2 - self.$sectionList.width());
-            }
-            self.$header.css("margin-left", "-450px");
-            self.$title.css("font-size", "80pt");
-            self.$contactList.css("right", (width - 900) / 2);
-        }
-        
-        $("#shadow").css("left", (width - $("#shadow").width()) / 2);
+        self.$title.css("font-size", $(this).width() < 900 ?
+                        $(this).width() / 11.25 + "pt" : "80pt");
     });
-
-    $("body")[0].addEventListener ("overflowchanged", function() {
-        var width = $(window).width();
-
-        $("#shadow").css("left", (width - $("#shadow").width()) / 2);
-        self.$contactList.css("right", (width - 900) / 2);
-        self.$sectionList.css("left", (width - 900) / 2 - self.$sectionList.width());
-    }, false);
 
     this.activeContent = this.sectionContents.about;
     this.activeTitle = this.sectionTitles.about;
     this.sectionCtrls["about"].click();
-        
+    
     $(window).resize();
 };
